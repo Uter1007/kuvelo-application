422d71a4ad429916f5a27ca556bc190d46a6a7e6
Christoph Ott <christoph.ott@lean-coders.at>
2015-10-12 12:44:04 +0200

    changed backed Rest API

--- index.js
+++ index.js
@@ -9,12 +9,30 @@ var express = require('express'),
     methodOverride = require('method-override'),
     bodyParser = require('body-parser');
 
+var locallydb = require('locallydb');
+
+// load the database (folder) in './mydb', will be created if doesn't exist
+var db = new locallydb('./mydb');
+
+// load the collection (file) in './mydb/messages', will be created if doesn't exist
+var collection = db.collection('messages');
+
+//Demodata
+collection.insert([
+    {name: "message1", description: "This is a Test 1"},
+    {name: "message2", description: "This is a Test 2"},
+    {name: "message3", description: "This is a Test 3"}
+]);
+
+collection.save();
+
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride());
 
 // Use helmet to secure Express headers
@@ -34,13 +52,47 @@ app.use(logger('dev'));
 app.use('/static',express.static(__dirname + '/public'));
 
 app.get('/api/messages' , function (req, res) {
-    fs.readFile( __dirname + "/" + "messages.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
-    });
+    res.send(collection.items);
 });
 
-app.get('/detail', function(req, res){
+app.get('/api/messages/:id' , function (req, res) {
+    var id = req.params.id;
+    var message = collection.get(id);
+    res.send(message);
+});
+
+app.post('/api/messages' , function (req, res) {
+    console.log(req.body);
+    var jsondata = JSON.parse(req.body);
+
+    collection.insert(jsondata);
+    collection.save();
+
+    res.end();
+
+});
+
+app.put('/api/messages/:id' , function (req, res) {
+    console.log(req.body);
+    var id = req.params.id;
+    var jsondata = JSON.parse(req.body);
+    collection.update(id, jsondata);
+    collection.save();
+
+    res.end();
+
+});
+
+app.delete('/api/messages/:id' , function (req, res) {
+    var id = req.params.id;
+    collection.remove(id);
+    collection.save();
+
+    res.end();
+});
+
+app.get('/detail/:id', function(req, res){
+    var id = req.params.id;
 
     res.render('detail.html', {
 
